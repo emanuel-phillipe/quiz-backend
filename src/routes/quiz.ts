cea144b496226661c4ae97a0c33db56c9e532eb3
Emanuel Phillipe Ribeiro <emanuelphillipe1706@gmail.com>
2024-10-23 20:12:10 -0300

    Added DELETE route

--- src/routes/quiz.ts
+++ src/routes/quiz.ts
@@ -15,6 +15,10 @@ const quiz_creation_body_zod_schema = z.object({
   }).array()
 })
 
+const quiz_remotion_body_zod_schema = z.object({
+  quizId: z.string()
+})
+
 export default async function quizRoutes(app:FastifyInstance){
 
   app.get("/all", {onRequest: [(app as any).authenticate]}, async (request:FastifyRequest, response) => {
@@ -48,6 +52,23 @@ export default async function quizRoutes(app:FastifyInstance){
     await prisma.question.createMany({data: questions})
 
     return response.status(201).send({"info": "Quiz created"})
+
+  })
+
+  app.delete("/delete", {onRequest: [(app as any).authenticate]}, async (request: FastifyRequest, response) => {
+
+    let requestBody = quiz_remotion_body_zod_schema.parse(request.body)
+    const requestUser = (request as any).user
+
+    const quizToDelete = await prisma.quiz.findFirst({where: {id: requestBody.quizId}})
+
+    if(!quizToDelete) return response.status(404).send({"info": "Quiz not found"})
+    if(quizToDelete.creatorId != requestUser.id || requestUser.type != "ADMIN") return response.status(401).send({"info": "User does not have permission to do this"})
+
+    await prisma.question.deleteMany({where: {quizId: quizToDelete.id}})
+    await prisma.quiz.delete({where: {id: quizToDelete.id}})
+
+    return response.status(200).send({"info": "Quiz deleted"})
 
   })
 }
